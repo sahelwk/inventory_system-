5ba04dd1b2253ed992f064431912d56ed7c8b8fc
sahel <mohabatullahsahel.afg@gmail.com>
2025-01-08 14:56:00 +0430

    first commit

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+use Illuminate\Support\Facades\DB;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+		DB::table('users')->insert([
+            [
+                'name'          => 'Admin',
+                'email'         => 'admin@gmail.com',
+                'password'      => bcrypt('sahel123'),
+                'created_at'    => date("Y-m-d H:i:s"),
+				'role'          => 'admin'
+            ],
+            [
+                'name'          => 'Sahel',
+                'email'         => 'sahel@mail.com',
+                'password'      => bcrypt('sahel123'),
+                'created_at'    => date("Y-m-d H:i:s"),
+				'role'          => 'staff'
+            ],
+        ]);
+    }
+}
